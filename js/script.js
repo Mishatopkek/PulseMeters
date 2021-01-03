aa5d0b64e18d0f22b293b1b5cdf7fc9f6abec107
Misha <Bezuglyyymisha@gmail.com>
2021-01-03 16:46:22 +0200

    Bug fix

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
         adaptiveHeight: true,
         // autoplay: true,
         // autoplaySpeed: 2000
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/carousel/Left.png"" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/carousel/Right.png"" alt=""></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/Left.png"" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/Right.png"" alt=""></button>',
         responsive: [
             {
                 breakpoint: 992,
